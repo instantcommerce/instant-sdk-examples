f37e9ad0f20fffbeec6f79fc08e991542e23da70
Aidana <aidana@storyofams.com>
2023-02-16 00:44:19 +0100

    feat(setup): potential customiser field mapping

---
+++ src/config/customiserFieldMapping.ts
@@ -0,0 +1,34 @@
+export const BASIC_SIZES = [
+  { label: 'Small', value: 'sm' },
+  { label: 'Medium', value: 'md' },
+  { label: 'Large', value: 'lg' }
+];
+
+// for when you want larger variants, e.g. for text
+export const BASIC_SIZES_LG = [
+  { label: 'Small', value: 'md' },
+  { label: 'Medium', value: 'lg' },
+  { label: 'Large', value: 'xl' }
+];
+
+export const BUTTON_VARIANTS = [
+  { label: 'Primary', value: 'primary' },
+  { label: 'Secondary', value: 'secondary' },
+  { label: 'Gray', value: 'gray' },
+  { label: 'Link', value: 'link' },
+  { label: 'Link primary', value: 'linkPrimary' },
+  { label: 'Link inverted', value: 'linkInverted' }
+];
+
+export const CONTAINER_WIDTHS = [
+  { label: 'Boxed', value: 'contained' },
+  { label: 'Full width', value: 'full' }
+];
+
+export const Theme = [
+  { label: 'Light', value: 'themeLight' },
+  { label: 'Gray', value: 'themeGray' },
+  { label: 'Primary light', value: 'themePrimaryLight' },
+  { label: 'Primary', value: 'themePrimary' },
+  { label: 'Dark', value: 'themeDark' }
+];
