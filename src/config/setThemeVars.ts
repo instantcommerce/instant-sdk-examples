97aa9d84b23e80bf5dfd1b507f031b73f8802153
Aidana <aidana@storyofams.com>
2023-02-16 00:42:59 +0100

    feat(setup): temp set theme vars

---
+++ src/config/setThemeVars.ts
@@ -0,0 +1,52 @@
+import { useTheme } from "@instantcommerce/sdk";
+
+// fallback primary colors
+const primaryColors = {
+  s50: '#F5FAFF',
+  s100: '#EFF8FF',
+  s200: '#D1E9FF',
+  s300: '#84CAFF',
+  s400: '#53B1FD',
+  s500: '#2E90FA',
+  s600: '#1570EF',
+  s700: '#175CD3',
+  s800: '#1849A9',
+  s900: '#0F2C60'
+};
+
+// fallback grayscale colors
+const grayscaleColors = {
+  s50: '#FAFAFA',
+  s100: '#F4F4F5',
+  s200: '#E4E4E7',
+  s300: '#D4D4D8',
+  s400: '#A1A1AA',
+  s500: '#71717A',
+  s600: '#52525B',
+  s700: '#3F3F46',
+  s800: '#27272A',
+  s900: '#18181B',
+};
+
+export const setThemeVars = () => {
+  const { colors } = useTheme();
+
+  // remove `s` to match tailwind colors (e.g. s500 -> 500)
+  const primaryObject = Object.fromEntries(
+    Object.entries(colors?.primary || primaryColors).map(([k, o]) => [
+      `--color-primary-${k.substring(1)}`,
+      o
+    ])
+  );
+  const grayscaleObject = Object.fromEntries(
+    Object.entries(colors?.grayscale || grayscaleColors).map(([k, o]) => [
+      `--color-gray-${k.substring(1)}`,
+      o
+    ])
+  );
+
+  return {
+    ...primaryObject,
+    ...grayscaleObject
+  };
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,34 +4,34 @@ module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      // colors: {
-      //   white: '#fff',
-      //   primary: {
-      //     50: 'var(--color-primary-50)',
-      //     100: 'var(--color-primary-100)',
-      //     200: 'var(--color-primary-200)',
-      //     300: 'var(--color-primary-300)',
-      //     400: 'var(--color-primary-400)',
-      //     500: 'var(--color-primary-500)',
-      //     600: 'var(--color-primary-600)',
-      //     700: 'var(--color-primary-700)',
-      //     800: 'var(--color-primary-800)',
-      //     900: 'var(--color-primary-900)'
-      //   },
-      //   gray: {
-      //     50: 'var(--color-gray-50)',
-      //     100: 'var(--color-gray-100)',
-      //     200: 'var(--color-gray-200)',
-      //     300: 'var(--color-gray-300)',
-      //     400: 'var(--color-gray-400)',
-      //     500: 'var(--color-gray-500)',
-      //     600: 'var(--color-gray-600)',
-      //     700: 'var(--color-gray-700)',
-      //     800: 'var(--color-gray-800)',
-      //     900: 'var(--color-gray-900)'
-      //   },
-      //   black: '#000',
-      // }
+      colors: {
+        white: '#fff',
+        primary: {
+          50: 'var(--color-primary-50)',
+          100: 'var(--color-primary-100)',
+          200: 'var(--color-primary-200)',
+          300: 'var(--color-primary-300)',
+          400: 'var(--color-primary-400)',
+          500: 'var(--color-primary-500)',
+          600: 'var(--color-primary-600)',
+          700: 'var(--color-primary-700)',
+          800: 'var(--color-primary-800)',
+          900: 'var(--color-primary-900)'
+        },
+        gray: {
+          50: 'var(--color-gray-50)',
+          100: 'var(--color-gray-100)',
+          200: 'var(--color-gray-200)',
+          300: 'var(--color-gray-300)',
+          400: 'var(--color-gray-400)',
+          500: 'var(--color-gray-500)',
+          600: 'var(--color-gray-600)',
+          700: 'var(--color-gray-700)',
+          800: 'var(--color-gray-800)',
+          900: 'var(--color-gray-900)'
+        },
+        black: '#000',
+      }
     },
     borderRadius: {
       DEFAULT: '4px',
