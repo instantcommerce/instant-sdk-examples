5ac5f7562c2a17581370007e2a4d263685cc1c3a
Aidana <aidana@storyofams.com>
2023-02-16 00:46:42 +0100

    feat(components): temp(?) title

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,55 @@
+import cx from 'classnames';
+import { AllHTMLAttributes, ElementType } from 'react';
+import {
+  displaySizeStyles,
+  headingSizeStyles,
+  titleVariants
+} from './titleStyles';
+
+type HTMLProperties = {
+  as?: ElementType;
+} & Omit<AllHTMLAttributes<HTMLElement>, 'color' | 'height' | 'width' | 'size'>;
+
+type TitleProps = {
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+  ellipsis?: boolean;
+  variant?: 'display' | 'heading';
+  uppercase?: boolean;
+} & HTMLProperties;
+
+export const Title = ({
+  as,
+  children,
+  className,
+  ellipsis,
+  size = 'md',
+  variant = 'heading',
+  uppercase = false,
+  ...props
+}: TitleProps) => {
+  const Component = as || 'h4';
+
+  return (
+    <Component
+      className={cx(
+        titleVariants[variant],
+        variant === 'heading'
+          ? headingSizeStyles[size]
+          : displaySizeStyles[size],
+        uppercase
+          ? `uppercase ${
+              variant === 'display' && size === 'xs'
+                ? 'tracking-wider'
+                : variant === 'heading' &&
+                  (size === 'sm' ? 'tracking-wide' : '')
+            }`
+          : '',
+        ellipsis ? 'truncate' : '',
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </Component>
+  );
+};
---
+++ src/components/Title/titleStyles.tsx
@@ -0,0 +1,22 @@
+export const titleVariants = {
+  display: 'tracking-tighter',
+  heading: 'tracking-tight'
+};
+
+export const headingSizeStyles = {
+  xs: 'text-xl leading-roomy',
+  sm: 'text-2xl leading-relaxed',
+  md: 'text-3xl leading-relaxed',
+  lg: 'text-4xl md:text-5xl leading-normal',
+  xl: 'text-5xl md:text-6xl leading-normal',
+  '2xl': 'text-6xl md:text-8xl leading-snug'
+};
+
+export const displaySizeStyles = {
+  xs: 'text-4xl md:text-5xl leading-normal',
+  sm: 'text-5xl md:text-6xl leading-normal',
+  md: 'text-6xl md:text-8xl leading-roomy',
+  lg: 'text-7xl md:text-10xl leading-roomy',
+  xl: 'text-8xl md:text-11xl leading-normal',
+  '2xl': 'text-9xl md:text-12xl leading-normal'
+};
