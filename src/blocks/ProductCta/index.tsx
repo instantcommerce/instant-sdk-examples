1457df3bbd9395e8dc970c7546e900c128c9649e
avizmarques <aviz.marques@gmail.com>
2023-02-28 19:10:29 +0100

    feat: style setup for product cta block

--- src/blocks/ProductCta/index.tsx
+++ src/blocks/ProductCta/index.tsx
@@ -1,27 +1,55 @@
 import { defineBlock, useBlockState } from "@instantcommerce/sdk";
 import cx from "classnames";
+import { Button, Paragraph, Title } from "../../components";
 
 import { setBlockTheme, setThemeColors } from "../../config";
 import "../../styles/global.css";
 
 const ProductCta = () => {
   const {
-    content,
-    customizer: { backgroundColor, theme, width },
+    content: { image },
+    customizer: { backgroundColor, theme, width, buttonType, imageAlignment },
   } = useBlockState();
 
   return (
-    <section className="product-cta px-2">
+    <section className={cx("product-cta", width === "contained" && "px-2")}>
       <div
-        className="section w-full bg-theme-bg"
+        className={cx(
+          "section w-full bg-theme-bg flex flex-col",
+          "h-[640px]",
+          imageAlignment === "right" ? "md:flex-row-reverse" : "md:flex-row"
+        )}
         style={{
           ...setThemeColors(),
           ...setBlockTheme(theme),
           ...(!!backgroundColor ? { backgroundColor } : {}),
         }}
       >
-        <div className="w-full h-full">image</div>
-        <div className="w-full h-full">content</div>
+        {!!image?.filename && (
+          <div
+            className="flex-1 bg-cover bg-center"
+            style={{ backgroundImage: `url(${image?.filename})` }}
+          />
+        )}
+
+        <div className="flex-1 h-full">
+          <div className="max-w-[352px] mx-auto h-full flex flex-col justify-center">
+            <Title as="h2" className="text-theme-title">
+              Minimal black sweater
+            </Title>
+
+            <Paragraph className="mt-1.5 text-theme-subtitle">
+              Our take on one of the classic essentials. Stylish, minimal,
+              got-to-have.
+            </Paragraph>
+
+            <div className="mt-1.5 text-theme-text">{"€20,00"}</div>
+
+            <Button variant={buttonType} className="mt-4">
+              Add to cart
+            </Button>
+          </div>
+        </div>
       </div>
     </section>
   );
@@ -50,12 +78,39 @@ export default defineBlock({
         ],
         preview: "contained",
       },
+      buttonType: {
+        type: "select",
+        options: [
+          { label: "Primary", value: "primary" },
+          { label: "Secondary", value: "secondary" },
+          { label: "Gray", value: "gray" },
+          { label: "Link", value: "link" },
+          { label: "Link primary", value: "linkPrimary" },
+          { label: "Link inverted", value: "linkInverted" },
+        ],
+        preview: "primary",
+      },
+      imageAlignment: {
+        type: "select",
+        options: [
+          { label: "Left", value: "left" },
+          { label: "Right", value: "right" },
+          { label: "Background", value: "bg" },
+        ],
+        preview: "left",
+      },
       backgroundColor: { type: "color", label: "Background color" },
     },
   },
   contentSchema: {
     fields: {
       title: { type: "text", label: "Title", preview: "Hero title" },
+      image: {
+        type: "image",
+        label: "Image",
+        preview:
+          "https://a.storyblok.com/f/145828/4424x3355/b22d1984af/force-majeure-ggpq78xm8t0-unsplash.jpg",
+      },
     },
   },
 });
