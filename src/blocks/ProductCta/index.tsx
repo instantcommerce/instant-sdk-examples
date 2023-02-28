c197d2b6a52aeb1e64f60e2931b187d8caad823b
avizmarques <aviz.marques@gmail.com>
2023-02-28 18:41:47 +0100

    feat: setup layout

--- src/blocks/ProductCta/index.tsx
+++ src/blocks/ProductCta/index.tsx
@@ -1,14 +1,29 @@
 import { defineBlock, useBlockState } from "@instantcommerce/sdk";
+import cx from "classnames";
+
+import { setBlockTheme, setThemeColors } from "../../config";
+import "../../styles/global.css";
 
 const ProductCta = () => {
-  const { content, customizer } = useBlockState();
+  const {
+    content,
+    customizer: { backgroundColor, theme, width },
+  } = useBlockState();
 
   return (
-    <div>
-      <h1 style={{ color: customizer.color }}>
-        {content.title}
-      </h1>
-    </div>
+    <section className="product-cta px-2">
+      <div
+        className="section w-full bg-theme-bg"
+        style={{
+          ...setThemeColors(),
+          ...setBlockTheme(theme),
+          ...(!!backgroundColor ? { backgroundColor } : {}),
+        }}
+      >
+        <div className="w-full h-full">image</div>
+        <div className="w-full h-full">content</div>
+      </div>
+    </section>
   );
 };
 
@@ -16,13 +31,31 @@ export default defineBlock({
   component: ProductCta,
   customizerSchema: {
     fields: {
-      color: { type: "color", label: "Color" },
+      theme: {
+        type: "select",
+        options: [
+          { label: "Light", value: "themeLight" },
+          { label: "Gray", value: "themeGray" },
+          { label: "Primary light", value: "themePrimaryLight" },
+          { label: "Primary", value: "themePrimary" },
+          { label: "Dark", value: "themeDark" },
+        ],
+        preview: "themeLight",
+      },
+      width: {
+        type: "select",
+        options: [
+          { label: "Boxed", value: "contained" },
+          { label: "Full width", value: "full" },
+        ],
+        preview: "contained",
+      },
+      backgroundColor: { type: "color", label: "Background color" },
     },
   },
   contentSchema: {
     fields: {
-      title: { type: "text", label: "Title", preview: 'Hero title' },
+      title: { type: "text", label: "Title", preview: "Hero title" },
     },
   },
 });
-;
