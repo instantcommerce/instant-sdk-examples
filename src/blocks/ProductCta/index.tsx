280c4ef6ae927d5f8cbe55dde912d8a0ddf9b950
avizmarques <aviz.marques@gmail.com>
2023-03-01 16:52:37 +0100

    feat: get shopify product data

--- src/blocks/ProductCta/index.tsx
+++ src/blocks/ProductCta/index.tsx
@@ -1,24 +1,105 @@
-import { defineBlock, useBlockState } from "@instantcommerce/sdk";
+import { useState, useEffect } from "react";
+import {
+  defineBlock,
+  useBlockState,
+  gql,
+  useShopifyClient,
+} from "@instantcommerce/sdk";
 import cx from "classnames";
 import { Button, Paragraph, Title } from "../../components";
 
 import { setBlockTheme, setThemeColors } from "../../config";
 import "../../styles/global.css";
 
+interface Product {
+  title: string;
+  description: string;
+  variants: {
+    edges: any[];
+  };
+}
+
+interface Response {
+  product: Product;
+}
+
+const productQuery = gql`
+  query getProductById($id: ID!) {
+    product(id: $id) {
+      title
+      description
+      ... on Product {
+        variants(first: 100) {
+          edges {
+            node {
+              id
+              price
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 const ProductCta = () => {
   const {
-    content: { image },
+    content: { productId, variantID, image },
     customizer: {
-      backgroundColor,
       theme,
+      variant,
       width,
       buttonType,
       imageAlignment,
       imageRatio,
       contentAlignment,
       textAlignment,
+      backgroundColor,
     },
   } = useBlockState();
+
+  const shopifyClient = useShopifyClient();
+
+  const [product, setProduct] = useState<Product>();
+  const [price, setPrice] = useState<string>("");
+
+  const loadProduct = async (id: string) => {
+    try {
+      const result = await shopifyClient.request<Response>(productQuery, {
+        id: `gid://shopify/Product/${id}`,
+      });
+
+      setProduct(result.product);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    if (!!productId) {
+      loadProduct(productId);
+    }
+  }, [productId]);
+
+  useEffect(() => {
+    if (!!product) {
+      const variant = !!variantID
+        ? product?.variants?.edges?.find(
+            (p) =>
+              p?.node?.id?.split("gid://shopify/ProductVariant/")?.[1] ===
+              variantID
+          )
+        : product?.variants?.edges?.[0]?.node;
+
+      setPrice(variant?.price);
+    }
+  }, [product]);
+
+  console.log(product);
+
+  // price is in what currency ? D:
+  // how to handle variants ?
+  // add to cart functionality missing
 
   return (
     <section className={cx("product-cta", width === "contained" && "px-2")}>
@@ -27,15 +108,20 @@ const ProductCta = () => {
           "section w-full bg-theme-bg flex flex-col",
           width === "contained" && "max-w-7xl mx-auto",
           // "h-[640px]",
-          imageAlignment === "right" ? "sm:flex-row-reverse" : "sm:flex-row"
+          imageAlignment === "right" && "sm:flex-row-reverse",
+          imageAlignment === "left" && "sm:flex-row",
+          imageAlignment === "bg" && "bg-cover py-[92px]"
         )}
         style={{
           ...setThemeColors(),
           ...setBlockTheme(theme),
           ...(!!backgroundColor ? { backgroundColor } : {}),
+          ...(imageAlignment === "bg"
+            ? { backgroundImage: `url(${image?.filename})` }
+            : {}),
         }}
       >
-        {!!image?.filename && (
+        {!!image?.filename && imageAlignment !== "bg" && (
           <div
             className={cx(
               "flex-1 bg-cover bg-center",
@@ -51,20 +137,20 @@ const ProductCta = () => {
             width === "full" && "sm:px-2",
             contentAlignment === "center" && "self-center",
             contentAlignment === "bottom" && "self-end",
-            textAlignment === "center" && "text-center"
+            textAlignment === "center" && "text-center",
+            variant === "block" && "bg-theme-bg"
           )}
         >
           <div className="max-w-[352px] mx-auto h-full flex flex-col justify-center">
             <Title as="h2" className="text-theme-title">
-              Minimal black sweater
+              {product?.title}
             </Title>
 
             <Paragraph className="mt-1.5 text-theme-subtitle">
-              Our take on one of the classic essentials. Stylish, minimal,
-              got-to-have.
+              {product?.description}
             </Paragraph>
 
-            <div className="mt-1.5 text-theme-text">{"€20,00"}</div>
+            <div className="mt-1.5 text-theme-text">{price}</div>
 
             <Button variant={buttonType} className="mt-4">
               Add to cart
@@ -90,6 +176,15 @@ export default defineBlock({
           { label: "Dark", value: "themeDark" },
         ],
         preview: "themeLight",
+      },
+      variant: {
+        type: "select",
+        options: [
+          { label: "Simple", value: "simple" },
+          { label: "Block overlay", value: "block" },
+          { label: "Overlay", value: "overlay" },
+        ],
+        preview: "simple",
       },
       width: {
         type: "select",
@@ -150,7 +245,16 @@ export default defineBlock({
   },
   contentSchema: {
     fields: {
-      title: { type: "text", label: "Title", preview: "Hero title" },
+      productId: {
+        type: "text",
+        label: "Product ID",
+        preview: "7433855598791",
+      },
+      variantId: {
+        type: "text",
+        label: "Variant ID",
+        preview: "42848196755655",
+      },
       image: {
         type: "image",
         label: "Image",
