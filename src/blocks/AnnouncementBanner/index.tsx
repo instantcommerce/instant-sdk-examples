6eb75dab2a1ab5e50e2c72bfd8f53e6edd393a8e
Aidana <aidana@storyofams.com>
2023-03-05 00:39:33 +0100

    feat(blocks): remaining formatted

--- src/blocks/AnnouncementBanner/index.tsx
+++ src/blocks/AnnouncementBanner/index.tsx
@@ -1,8 +1,4 @@
-import {
-  defineBlock,
-  useBlockState,
-  Link,
-} from "@instantcommerce/sdk";
+import { defineBlock, useBlockState, Link } from "@instantcommerce/sdk";
 import cx from "classnames";
 import { useState } from "react";
 import { Paragraph } from "../../components";
@@ -24,8 +20,8 @@ const AnnouncementBanner = () => {
       backgroundColor,
       textColor,
       linkColor,
-      closeColor,
-    },
+      closeColor
+    }
   } = useBlockState();
 
   const [dismissed, setDismissed] = useState<boolean>(false);
@@ -34,7 +30,7 @@ const AnnouncementBanner = () => {
     <div
       className={cx(
         "announcement-banner relative w-full bg-theme-bg py-1.25 text-sm",
-        alignment == "center" ? 'text-center' : 'text-left',
+        alignment == "center" ? "text-center" : "text-left",
         dismissed && "hidden",
         backgroundImage && "bg-cover"
       )}
@@ -44,13 +40,15 @@ const AnnouncementBanner = () => {
         ...(!!backgroundColor ? { backgroundColor } : {}),
         ...(!!backgroundImage
           ? { backgroundImage: `url(${image?.filename})` }
-          : {}),
+          : {})
       }}
     >
       <div
         className={cx(
           "announcement-banner__text-wrapper flex flex-col sm:flex-row mx-auto justify-center",
-          alignment == "center" ? "sm:justify-center px-6" : "sm:justify-start pl-2 pr-6 md:pl-6"
+          alignment == "center"
+            ? "sm:justify-center px-6"
+            : "sm:justify-start pl-2 pr-6 md:pl-6"
         )}
       >
         {(!!text || !!links?.[0]?.value?.label) && (
@@ -63,7 +61,10 @@ const AnnouncementBanner = () => {
               // @ts-ignore
               <Link
                 to={links[0]?.value?.link}
-                className={cx("announcement-banner__link inline sm:ml-1 text-sm font-medium underline text-theme-link transition-opacity hover:opacity-70", text ? 'ml-0.5' : '')}
+                className={cx(
+                  "announcement-banner__link inline sm:ml-1 text-sm font-medium underline text-theme-link transition-opacity hover:opacity-70",
+                  text ? "ml-0.5" : ""
+                )}
                 style={{ ...(!!linkColor ? { color: linkColor } : {}) }}
               >
                 {links[0].value.label}
@@ -71,7 +72,6 @@ const AnnouncementBanner = () => {
             )}
           </Paragraph>
         )}
-
       </div>
 
       {!!dismissable && (
@@ -98,38 +98,38 @@ export default defineBlock({
           { label: "Gray", value: "themeGray" },
           { label: "Primary light", value: "themePrimaryLight" },
           { label: "Primary", value: "themePrimary" },
-          { label: "Dark", value: "themeDark" },
+          { label: "Dark", value: "themeDark" }
         ],
-        preview: "themeGray",
+        preview: "themeGray"
       },
       alignment: {
         type: "select",
         options: [
           { label: "Left", value: "left" },
-          { label: "Center", value: "center" },
+          { label: "Center", value: "center" }
         ],
-        preview: "center",
+        preview: "center"
       },
       dismissable: {
         type: "toggle",
-        preview: false,
+        preview: false
       },
       backgroundImage: {
         type: "toggle",
-        preview: false,
+        preview: false
       },
       backgroundColor: { type: "color", label: "Background color" },
       textColor: { type: "color", label: "Text color" },
       linkColor: { type: "color", label: "Link color" },
-      closeColor: { type: "color", label: "Close button color" },
-    },
+      closeColor: { type: "color", label: "Close button color" }
+    }
   },
   contentSchema: {
     fields: {
       text: {
         type: "text",
         label: "Text",
-        preview: "Welcome to our new site!",
+        preview: "Welcome to our new site!"
       },
       links: {
         type: "subschema",
@@ -138,16 +138,16 @@ export default defineBlock({
         preview: [
           {
             subschema: "link",
-            value: { label: "Learn more", link: "https://instantcommerce.io/" },
-          },
-        ],
+            value: { label: "Learn more", link: "https://instantcommerce.io/" }
+          }
+        ]
       },
       image: {
         type: "image",
         label: "Background image",
         preview:
-          "https://a.storyblok.com/f/145828/1440x40/e5735c2906/autumn.png",
-      },
+          "https://a.storyblok.com/f/145828/1440x40/e5735c2906/autumn.png"
+      }
     },
     subschemas: {
       link: {
@@ -156,16 +156,16 @@ export default defineBlock({
             type: "text",
             label: "Label",
             isTranslatable: true,
-            isRequired: true,
+            isRequired: true
           },
           link: {
             type: "text",
             label: "Link",
             isTranslatable: true,
-            isRequired: true,
-          },
-        },
-      },
-    },
-  },
+            isRequired: true
+          }
+        }
+      }
+    }
+  }
 });
