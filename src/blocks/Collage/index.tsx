661bd0c1fa46918d7c1ca53f39e607fbe0094dcb
avizmarques <aviz.marques@gmail.com>
2023-03-07 12:45:50 +0100

    fix: import of global styles

--- src/blocks/Collage/index.tsx
+++ src/blocks/Collage/index.tsx
@@ -4,7 +4,7 @@ import cx from "classnames";
 import { Container, Overlay, Paragraph, Title } from "../../components";
 import { ArrowRightIcon } from "../../components/Icons";
 import { setBlockTheme, setThemeColors } from "../../config";
-import "../../styles/global.css";
+import "../../styles/global.scss";
 import "./collage.scss";
 
 const Collage = () => {
@@ -24,7 +24,7 @@ const Collage = () => {
       linkColor,
       linkType,
       ...headerCustomizations
-    }
+    },
   } = useBlockState();
 
   return (
@@ -118,34 +118,34 @@ export default defineBlock({
           { label: "Gray", value: "themeGray" },
           { label: "Primary light", value: "themePrimaryLight" },
           { label: "Primary", value: "themePrimary" },
-          { label: "Dark", value: "themeDark" }
+          { label: "Dark", value: "themeDark" },
         ],
-        preview: "themeLight"
+        preview: "themeLight",
       },
       width: {
         type: "select",
         options: [
           { label: "Boxed", value: "contained" },
-          { label: "Full width", value: "full" }
+          { label: "Full width", value: "full" },
         ],
-        preview: "contained"
+        preview: "contained",
       },
       headerAlignment: {
         type: "select",
         options: [
           { label: "Left", value: "left" },
-          { label: "Center", value: "center" }
+          { label: "Center", value: "center" },
         ],
-        preview: "left"
+        preview: "left",
       },
       headerSize: {
         type: "select",
         options: [
           { label: "Small", value: "md" },
           { label: "Medium", value: "lg" },
-          { label: "Large", value: "xl" }
+          { label: "Large", value: "xl" },
         ],
-        preview: "lg"
+        preview: "lg",
       },
       pretitleColor: { type: "color", label: "Pretitle color" },
       titleColor: { type: "color", label: "Title color" },
@@ -159,9 +159,9 @@ export default defineBlock({
           { label: "Gray", value: "gray" },
           { label: "Link", value: "link" },
           { label: "Link primary", value: "linkPrimary" },
-          { label: "Link inverted", value: "linkInverted" }
+          { label: "Link inverted", value: "linkInverted" },
         ],
-        preview: "link"
+        preview: "link",
       },
       buttonCorners: {
         type: "select",
@@ -170,26 +170,26 @@ export default defineBlock({
           { label: "Small", value: "xs" },
           { label: "Medium", value: "md" },
           { label: "Large", value: "lg" },
-          { label: "Full", value: "full" }
+          { label: "Full", value: "full" },
         ],
-        preview: "none"
+        preview: "none",
       },
       buttonWeight: {
         type: "select",
         options: [
           { label: "Regular", value: "base" },
           { label: "Medium", value: "medium" },
-          { label: "Bold", value: "bold" }
+          { label: "Bold", value: "bold" },
         ],
-        preview: "medium"
+        preview: "medium",
       },
       buttonLocation: {
         type: "select",
         options: [
           { label: "Top", value: "top" },
-          { label: "Bottom", value: "bottom" }
+          { label: "Bottom", value: "bottom" },
         ],
-        preview: "top"
+        preview: "top",
       },
       dividerColor: { type: "color", label: "Divider color" },
       hasDivider: { type: "toggle", label: "Has divider", preview: false },
@@ -198,30 +198,30 @@ export default defineBlock({
         options: [
           { label: "Small", value: "sm" },
           { label: "Medium", value: "md" },
-          { label: "Large", value: "lg" }
+          { label: "Large", value: "lg" },
         ],
-        preview: "md"
+        preview: "md",
       },
       overlayColor: {
         type: "color",
         label: "Image overlay color",
-        preview: "#000"
+        preview: "#000",
       },
       overlayOpacity: {
         type: "number",
         label: "Image overlay opacity",
         min: 0,
         max: 100,
-        preview: 40
+        preview: 40,
       },
       contentAlignment: {
         type: "select",
         label: "Card content alignment",
         options: [
           { label: "Left", value: "left" },
-          { label: "Center", value: "center" }
+          { label: "Center", value: "center" },
         ],
-        preview: "left"
+        preview: "left",
       },
       contentSize: {
         type: "select",
@@ -229,18 +229,18 @@ export default defineBlock({
         options: [
           { label: "Small", value: "sm" },
           { label: "Medium", value: "md" },
-          { label: "Large", value: "lg" }
+          { label: "Large", value: "lg" },
         ],
-        preview: "md"
+        preview: "md",
       },
       mobileLayout: {
         type: "select",
         label: "Mobile layout",
         options: [
           { label: "Collage", value: "collage" },
-          { label: "Column", value: "column" }
+          { label: "Column", value: "column" },
         ],
-        preview: "column"
+        preview: "column",
       },
       textColor: { type: "color", label: "Text color" },
       linkColor: { type: "color", label: "Link color" },
@@ -250,30 +250,30 @@ export default defineBlock({
         options: [
           { label: "None", value: "none" },
           { label: "Left", value: "left" },
-          { label: "Right", value: "right" }
+          { label: "Right", value: "right" },
         ],
-        preview: "left"
-      }
-    }
+        preview: "left",
+      },
+    },
   },
   contentSchema: {
     fields: {
       pretitle: {
         type: "text",
         label: "Pretitle",
-        isTranslatable: true
+        isTranslatable: true,
       },
       title: {
         type: "text",
         label: "Title",
         preview: "High tech performancewear",
-        isTranslatable: true
+        isTranslatable: true,
       },
       subtitle: {
         type: "text",
         label: "Description",
         preview: "Clothing that is as amibitious as you are.",
-        isTranslatable: true
+        isTranslatable: true,
       },
       buttons: {
         type: "subschema",
@@ -284,17 +284,17 @@ export default defineBlock({
             subschema: "button",
             value: {
               text: "View collections",
-              link: "https://instantcommerce.io/"
-            }
-          }
-        ]
+              link: "https://instantcommerce.io/",
+            },
+          },
+        ],
       },
       cta: {
         type: "text",
         label: "CTA text",
         preview: "Discover the collection",
         isTranslatable: true,
-        isRequired: true
+        isRequired: true,
       },
       cards: {
         type: "subschema",
@@ -307,8 +307,8 @@ export default defineBlock({
               image:
                 "https://a.storyblok.com/f/145828/5104x3403/ea3b04ac71/force-majeure-vujikv6pbjq-unsplash.jpg",
               title: "FW22 WOMEN",
-              link: "https://instantcommerce.io/"
-            }
+              link: "https://instantcommerce.io/",
+            },
           },
           {
             subschema: "card",
@@ -316,8 +316,8 @@ export default defineBlock({
               image:
                 "https://a.storyblok.com/f/145828/4390x3245/6fa2d27260/force-majeure-8eu-hahcrhk-unsplash.jpg",
               title: "FW22 MEN",
-              link: "https://instantcommerce.io/"
-            }
+              link: "https://instantcommerce.io/",
+            },
           },
           {
             subschema: "card",
@@ -325,11 +325,11 @@ export default defineBlock({
               image:
                 "https://a.storyblok.com/f/145828/4424x3355/b22d1984af/force-majeure-ggpq78xm8t0-unsplash.jpg",
               title: "FW22 KIDS",
-              link: "https://instantcommerce.io/"
-            }
-          }
-        ]
-      }
+              link: "https://instantcommerce.io/",
+            },
+          },
+        ],
+      },
     },
     subschemas: {
       button: {
@@ -339,15 +339,15 @@ export default defineBlock({
             label: "Text",
             isTranslatable: true,
             isRequired: true,
-            maxLength: 40
+            maxLength: 40,
           },
           link: {
             type: "link",
             label: "Link",
             isTranslatable: true,
-            isRequired: true
-          }
-        }
+            isRequired: true,
+          },
+        },
       },
       card: {
         fields: {
@@ -355,22 +355,22 @@ export default defineBlock({
             type: "image",
             label: "Image",
             isTranslatable: false,
-            isRequired: false
+            isRequired: false,
           },
           title: {
             type: "text",
             label: "Title",
             isTranslatable: true,
-            isRequired: true
+            isRequired: true,
           },
           link: {
             type: "link",
             label: "Link",
             isTranslatable: true,
-            isRequired: true
-          }
-        }
-      }
-    }
-  }
+            isRequired: true,
+          },
+        },
+      },
+    },
+  },
 });
