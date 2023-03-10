4a6af3cd75143c4862a58f4e77fb078b375d5960
avizmarques <aviz.marques@gmail.com>
2023-03-10 16:18:30 +0100

    refactor: announcement banner with tailwind spacing

--- src/blocks/AnnouncementBanner/index.tsx
+++ src/blocks/AnnouncementBanner/index.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import { defineBlock, useBlockState, Link } from "@instantcommerce/sdk";
-import cx from "classnames";
+import { useState } from 'react';
+import { defineBlock, useBlockState, Link } from '@instantcommerce/sdk';
+import cx from 'classnames';
 
-import { Paragraph } from "~/components";
-import { CloseIcon } from "~/components/Icons";
-import { setThemeColors, setBlockTheme } from "~/config";
-import "~/styles/global.scss";
+import { Paragraph } from '~/components';
+import { CloseIcon } from '~/components/Icons';
+import { setThemeColors, setBlockTheme } from '~/config';
+import '~/styles/global.scss';
 
 const AnnouncementBanner = () => {
   const {
@@ -18,8 +18,8 @@ const AnnouncementBanner = () => {
       backgroundColor,
       textColor,
       linkColor,
-      closeColor
-    }
+      closeColor,
+    },
   } = useBlockState();
 
   const [dismissed, setDismissed] = useState<boolean>(false);
@@ -27,20 +27,20 @@ const AnnouncementBanner = () => {
   return (
     <div
       className={cx(
-        "announcement-banner__container bg-theme-bg relative w-full py-1.25 text-sm font-medium",
-        alignment == "center" ? "text-center" : "text-left",
-        dismissed && "hidden"
+        'announcement-banner__container bg-theme-bg relative w-full py-2.5 text-sm font-medium',
+        alignment == 'center' ? 'text-center' : 'text-left',
+        dismissed && 'hidden'
       )}
       style={{
         ...setThemeColors(),
         ...setBlockTheme(theme),
-        ...(!!backgroundColor ? { backgroundColor } : {})
+        ...(!!backgroundColor ? { backgroundColor } : {}),
       }}
     >
       {!!hasBackgroundImage && image?.filename && (
         <img
           className={cx(
-            "absolute top-0 left-0 right-0 bottom-0 w-0 h-0 min-w-full max-w-full min-h-full max-h-0 object-cover"
+            'absolute top-0 left-0 right-0 bottom-0 w-0 h-0 min-w-full max-w-full min-h-full max-h-0 object-cover'
           )}
           alt={image?.alt}
           src={image.filename}
@@ -49,10 +49,10 @@ const AnnouncementBanner = () => {
 
       <div
         className={cx(
-          "announcement-banner relative flex justify-center flex-col sm:flex-row z-10",
-          alignment == "center"
-            ? "sm:justify-center px-6"
-            : "sm:justify-start pl-2 pr-6 md:pl-6"
+          'announcement-banner relative flex justify-center flex-col sm:flex-row z-10',
+          alignment == 'center'
+            ? 'sm:justify-center px-12'
+            : 'sm:justify-start pl-4 pr-12 md:pl-12'
         )}
       >
         {(!!text || !!links?.[0]?.value?.label) && (
@@ -66,8 +66,8 @@ const AnnouncementBanner = () => {
               <Link
                 to={links[0]?.value?.link}
                 className={cx(
-                  "announcement-banner__link text-theme-link inline sm:ml-1 underline transition-opacity hover:opacity-70",
-                  text ? "ml-0.5" : ""
+                  'announcement-banner__link text-theme-link inline sm:ml-2 underline transition-opacity hover:opacity-70',
+                  text ? 'ml-1' : ''
                 )}
                 style={{ ...(!!linkColor ? { color: linkColor } : {}) }}
               >
@@ -81,7 +81,7 @@ const AnnouncementBanner = () => {
       {!!dismissable && (
         <button
           onClick={() => setDismissed(true)}
-          className="announcement-banner__icon text-theme-icon w-2.5 absolute right-2 top-1/2 -translate-y-1/2"
+          className="announcement-banner__icon text-theme-icon w-5 absolute right-4 top-1/2 -translate-y-1/2"
           style={{ ...(!!closeColor ? { color: closeColor } : {}) }}
         >
           <CloseIcon />
@@ -96,80 +96,80 @@ export default defineBlock({
   customizerSchema: {
     fields: {
       theme: {
-        type: "select",
+        type: 'select',
         options: [
-          { label: "Light", value: "themeLight" },
-          { label: "Gray", value: "themeGray" },
-          { label: "Primary light", value: "themePrimaryLight" },
-          { label: "Primary", value: "themePrimary" },
-          { label: "Dark", value: "themeDark" }
+          { label: 'Light', value: 'themeLight' },
+          { label: 'Gray', value: 'themeGray' },
+          { label: 'Primary light', value: 'themePrimaryLight' },
+          { label: 'Primary', value: 'themePrimary' },
+          { label: 'Dark', value: 'themeDark' },
         ],
-        preview: "themeGray"
+        preview: 'themeGray',
       },
       alignment: {
-        type: "select",
+        type: 'select',
         options: [
-          { label: "Left", value: "left" },
-          { label: "Center", value: "center" }
+          { label: 'Left', value: 'left' },
+          { label: 'Center', value: 'center' },
         ],
-        preview: "center"
+        preview: 'center',
       },
       dismissable: {
-        type: "toggle",
-        preview: false
+        type: 'toggle',
+        preview: false,
       },
       hasBackgroundImage: {
-        type: "toggle",
-        preview: false
+        type: 'toggle',
+        preview: false,
       },
-      backgroundColor: { type: "color", label: "Background color" },
-      textColor: { type: "color", label: "Text color" },
-      linkColor: { type: "color", label: "Link color" },
-      closeColor: { type: "color", label: "Close button color" }
-    }
+      backgroundColor: { type: 'color', label: 'Background color' },
+      textColor: { type: 'color', label: 'Text color' },
+      linkColor: { type: 'color', label: 'Link color' },
+      closeColor: { type: 'color', label: 'Close button color' },
+    },
   },
   contentSchema: {
     fields: {
       text: {
-        type: "text",
-        label: "Text",
-        preview: "Welcome to our new site!"
+        type: 'text',
+        label: 'Text',
+        preview: 'Welcome to our new site!',
       },
       links: {
-        type: "subschema",
-        allowed: ["link"],
+        type: 'subschema',
+        allowed: ['link'],
         max: 1,
         preview: [
           {
-            subschema: "link",
-            value: { label: "Learn more", link: "https://instantcommerce.io/" }
-          }
-        ]
+            subschema: 'link',
+            value: { label: 'Learn more', link: 'https://instantcommerce.io/' },
+          },
+        ],
       },
       image: {
-        type: "image",
-        label: "Background image",
+        type: 'image',
+        label: 'Background image',
         preview:
-          "https://a.storyblok.com/f/145828/5085x162/8285ff8859/force-majeure-vujikv6pbjq-unsplash.jpg"
-      }
+          'https://a.storyblok.com/f/145828/5085x162/8285ff8859/force-majeure-vujikv6pbjq-unsplash.jpg',
+      },
     },
     subschemas: {
       link: {
         fields: {
           label: {
-            type: "text",
-            label: "Label",
+            type: 'text',
+            label: 'Label',
             isTranslatable: true,
-            isRequired: true
+            isRequired: true,
           },
           link: {
-            type: "text",
-            label: "Link",
+            type: 'text',
+            label: 'Link',
             isTranslatable: true,
-            isRequired: true
-          }
-        }
-      }
-    }
-  }
+            isRequired: true,
+          },
+        },
+      },
+    },
+  },
 });
