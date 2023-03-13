0bbb4fc1a94ea2bfdd16dd7689dec6dd63b5c417
Aidana <aidana@storyofams.com>
2023-03-13 17:24:26 +0100

    refactor(button): rename gray to light & light inverted to light

--- src/components/Button/buttonStyles.ts
+++ src/components/Button/buttonStyles.ts
@@ -6,12 +6,13 @@ export const variantStyles = {
     "bg-primary-500 border-primary-500 text-white hover:bg-primary-700 hover:border-primary-700 focus:shadow-[0px_0px_0px_4px_var(--color-primary-200)] disabled:bg-primary-200 disabled:border-primary-200",
   secondary:
     "bg-white border-primary-100 text-primary-600 hover:bg-primary-200 hover:border-primary-200 focus:shadow-[0px_0px_0px_4px_var(--color-primary-200)] disabled:bg-primary-50 disabled:border-primary-50",
-  gray: "bg-white border-gray-300 text-gray-700 hover:bg-primary-50 focus:shadow-[0px_0px_0px_4px_var(--color-primary-100)] disabled:bg-gray-200 disabled:border-gray-200",
+  light:
+    "bg-white border-gray-300 text-gray-700 hover:bg-primary-50 focus:shadow-[0px_0px_0px_4px_var(--color-primary-100)] disabled:bg-gray-200 disabled:border-gray-200",
   dark: "bg-gray-800 border-gray-300 text-white hover:bg-black focus:shadow-[0px_0px_0px_4px_var(--color-primary-100)] disabled:bg-gray-700 disabled:border-gray-700",
   link: "text-gray-700 hover:text-gray-500 focus:text-gray-500 disabled:text-gray-300",
   linkPrimary:
     "text-primary-600 hover:text-primary-700 focus:text-primary-700 disabled:text-gray-300",
-  linkInverted:
+  linkLight:
     "text-white hover:text-gray-200 focus:text-gray-200 disabled:text-gray-200",
   unstyled: "",
 };
